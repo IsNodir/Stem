835ad397a2d873ded936e310b75c038b5f44ea4c
EgamberdiJabborov <jabborovegamberdi2002@gmail.com>
2023-06-30 22:45:52 +0500

    classes updated

--- src/main/java/itmasters/project/stem/security/config/JwtService.java
+++ src/main/java/itmasters/project/stem/security/config/JwtService.java
@@ -42,7 +42,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 2400))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 240))
                 .signWith(getSingInKey(), SignatureAlgorithm.HS256)
                 .compact(); //will generate and return the token
     }
