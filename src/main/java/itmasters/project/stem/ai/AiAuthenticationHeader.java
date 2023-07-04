a1e76de10c3cbb3bfe4b262b36703e9575ccad35
IsNodir <blacklord268@gmail.com>
2023-07-04 15:52:46 +0500

    Jar

--- src/main/java/itmasters/project/stem/ai/AiAuthenticationHeader.java
+++ src/main/java/itmasters/project/stem/ai/AiAuthenticationHeader.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class AiAuthenticationHeader {
 
-    private final String aiApiKey = "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";
+    private final String aiApiKey = "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";
 
     @Bean
     public RestTemplate template(){
