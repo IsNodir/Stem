53cbfc46821b948759e086f3b46c819ebba2815a
IsNodir <blacklord268@gmail.com>
2023-07-04 15:25:12 +0500

    Jar

--- src/main/java/itmasters/project/stem/translator/AuthenticationHeader.java
+++ src/main/java/itmasters/project/stem/translator/AuthenticationHeader.java
@@ -23,7 +23,7 @@ public class AuthenticationHeader {
 
     public TranslatorResponse EnUz(String text) {
 
-        final String key = "trnsl.1.1.20230610T083326Z.91a3793f9beee2a0.fe3528a48df9ef771c6d885d708ac8c33c234475";
+        final String key = "trnsl.1.1.20230704T102321Z.80a04aaf9f40adf8.d11e7af3845904f57514028d0c9cff4239438334";
         final String lang = "en-uz";
         final String baseUrl = String.format("https://translate.yandex.net/api/v1.5/tr.json/translate?key=%s&text=%s&lang=%s", key, text, lang);
         WebClient.Builder webClient = WebClient.builder();
